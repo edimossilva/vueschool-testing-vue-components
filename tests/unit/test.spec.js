d4273a0b200ed2c6cf6beb565caecceef0d9584a
edimossilva <edimossilva@gmail.com>
2019-10-12 17:03:49 +1300

    shallow and shallowMount using props

--- tests/unit/test.spec.js
+++ tests/unit/test.spec.js
@@ -1,4 +1,18 @@
+import TestComponent from '@/test'
+import List from '@/list'
 
-it('description', () => {
-  expect(true).toBe(!false)
-});
+import { mount, shallowMount } from '@vue/test-utils'
+
+test('mount a vue component', () => {
+  const wrapper = mount(TestComponent, {
+    propsData: {
+      value: 'VueSchool2'
+    }
+  })
+  expect(wrapper.html()).toMatchSnapshot()
+})
+
+test('ListComponent Shallow', () => {
+  console.log(mount(List).html())
+  console.log(shallowMount(List).html())
+})
