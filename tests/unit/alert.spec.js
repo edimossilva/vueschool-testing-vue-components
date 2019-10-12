a28f79c10fa7089747a10ec24093d9a2b7b55a5f
edimossilva <edimossilva@gmail.com>
2019-10-13 11:53:50 +1300

    Testing lifecycles methods with spy methods and using faker time

--- tests/unit/alert.spec.js
+++ tests/unit/alert.spec.js
@@ -1,6 +1,29 @@
 import AlertMessage from '@/alert-message'
 import { mount } from '@vue/test-utils'
 
-it('description', () => {
-  expect(true).toBe(!false)
+
+it('mounted assigns interval', () => {
+  const wrapper = mount(AlertMessage)
+  const vm = wrapper.vm
+  expect(vm.interval).not.toBe(undefined)
+});
+
+it('counter works', () => {
+  jest.useFakeTimers()
+  const wrapper = mount(AlertMessage)
+  const vm = wrapper.vm
+  expect(vm.counter).toEqual(0)
+  jest.advanceTimersByTime(1000)
+  expect(vm.counter).toEqual(1)
+  jest.advanceTimersByTime(1000)
+  expect(vm.counter).toEqual(2)
+});
+
+it('instances get destroyed', () => {
+  const beforeDestroyedSpy = jest.spyOn(AlertMessage,'beforeDestroy')
+  const wrapper = mount(AlertMessage)
+  const vm = wrapper.vm
+  vm.counter = vm.timer - 1
+  jest.advanceTimersByTime(1000)
+  expect(beforeDestroyedSpy).toBeCalledTimes(1)
 });
