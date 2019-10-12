a3b69555d38f07550a9b0f099b5b3c712c02ca6f
edimossilva <edimossilva@gmail.com>
2019-10-13 11:10:20 +1300

    adding tests to async method s

--- tests/unit/github.spec.js
+++ tests/unit/github.spec.js
@@ -2,6 +2,31 @@ import GithubCard from '@/github-card'
 import { mount } from '@vue/test-utils'
 
 
-it('description', () => {
-  expect(true).toBe(!false)
+describe('GithubCard', () => {
+  it('composeUrl', () => {
+    const wrapper = mount(GithubCard)
+    const vm = wrapper.vm;
+    expect(vm.composeUrl('mock')).toBe('https://api.github.com/users/mock');
+    expect(vm.composeUrl(123)).toBe('https://api.github.com/users/123');
+  });
+
+  it('fetchData', async () => {
+    const jsonMock = jest.fn().mockResolvedValue('data')
+    const wrapper = mount(GithubCard);
+    const vm = wrapper.vm;
+
+    window.fetch = jest.fn().mockResolvedValue({
+      json: jsonMock
+    })
+
+    wrapper.setMethods({
+      composeUrl: () => 'mock_url'
+    })
+
+    await vm.fetchData()
+    expect(window.fetch).toHaveBeenCalledWith('mock_url')
+    expect(jsonMock).toHaveBeenCalled()
+    expect(vm.data).toEqual('data')
+
+  });
 });
