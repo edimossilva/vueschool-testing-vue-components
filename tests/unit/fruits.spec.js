78f00cb1d1a4d596cf9ac9a87f0242aeb9f6b982
edimossilva <edimossilva@gmail.com>
2019-10-13 14:23:37 +1300

    acessing dom events

--- tests/unit/fruits.spec.js
+++ tests/unit/fruits.spec.js
@@ -1,7 +1,28 @@
 import  FruitBasket from '@/fruit-basket'
 import { mount } from '@vue/test-utils'
+import { domainToASCII } from 'url';
+
+it('fills fruit', () => {
+  const wrapper = mount(FruitBasket)
+  const inputDOM = wrapper.find('input')
+  inputDOM.element.value = "banana"
+  inputDOM.trigger('input')
+  expect(wrapper.vm.fruit).toEqual("banana")
+});
 
 
-it('description', () => {
-  expect(true).toBe(!false)
+it('add fruit on button click', () => {
+
+  const wrapper = mount(FruitBasket)
+
+  const inputDOM = wrapper.find('input')
+  inputDOM.element.value = "banana"
+  inputDOM.trigger('input')
+
+  const buttonDOM = wrapper.find('button')
+  buttonDOM.trigger('click')
+
+  expect(wrapper.vm.fruit).toEqual("")
+  expect(wrapper.vm.basket).toEqual(expect.arrayContaining(['banana']))
+  expect(wrapper.findAll('li').length).toBe(1)
 });
