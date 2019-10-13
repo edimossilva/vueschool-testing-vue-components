df25956b307be763454c1a3f8f7d139b5bdcf88a
edimossilva <edimossilva@gmail.com>
2019-10-13 17:30:48 +1300

    adding tests for store

--- tests/unit/salad.spec.js
+++ tests/unit/salad.spec.js
@@ -6,7 +6,24 @@ import SaladBowlComponent from '@/salad-bowl'
 import saladStore from '@/store/salad-store'
 
 
+Vue.use(Vuex)
 
-it('description', () => {
-  expect(true).toBe(!false)
+it('store is loaded', () => {
+  const store = new Vuex.Store(saladStore)
+  const wrapper = mount(SaladBowlComponent,{
+    store
+  })
+
+  store.state.salad.push('cucumber')
+  expect(wrapper.vm.salad).toEqual(['cucumber'])
+});
+
+it('store works', () => {
+  const store = new Vuex.Store(saladStore)
+  const wrapper = mount(SaladBowlComponent,{
+    store
+  })
+
+  wrapper.vm.addIngredient('cucumber')
+  expect(wrapper.vm.salad).toEqual(['cucumber'])
 });
